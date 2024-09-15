b989ced587cab0bf1105b581eb945e99ac78eaad
undefined1raven <zlat.dominic.francisc@gmail.com>
2024-09-15 11:21:28 +0300

    testing account UI

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -9,6 +9,8 @@
 	import { createClient } from '@supabase/supabase-js';
 	import isMobile from '../../fn/isMobile';
 	import displayName from '../../stores/displayName';
+	import globalStyle from '../../stores/globalStyles';
+	import getDateFromUnix from '../../fn/getDateFromUnix';
 	let fileInput;
 	const client = createClient(
 		'https://wrbgbsulbyytggffcavl.supabase.co/', ///safe. its read only on purpose
@@ -34,8 +36,41 @@
 	$: figmaImportConfig = isMobile()
 		? { containerHeight: window.screen.height, containerWidth: window.screen.width }
 		: { containerHeight: 387, containerWidth: 585 };
+
+	const collection = [
+		{ id: '10', path: '', name: '', tx: Date.now() },
+		{ id: '111', path: '', name: '', tx: Date.now() },
+		{ id: '5434', path: '', name: '', tx: Date.now() },
+		{ id: '35', path: '', name: '', tx: Date.now() },
+		{ id: '321', path: '', name: '', tx: Date.now() },
+		{ id: '23', path: '', name: '', tx: Date.now() },
+		{ id: '885', path: '', name: '', tx: Date.now() },
+		{ id: '658', path: '', name: '', tx: Date.now() },
+		{ id: '234', path: '', name: '', tx: Date.now() },
+		{ id: '684', path: '', name: '', tx: Date.now() },
+		{ id: '765', path: '', name: '', tx: Date.now() },
+		{ id: '65', path: '', name: '', tx: Date.now() },
+		{ id: '2332', path: '', name: '', tx: Date.now() },
+		{ id: '654', path: '', name: '', tx: Date.now() },
+		{ id: '534', path: '', name: '', tx: Date.now() },
+		{ id: '4', path: '', name: '', tx: Date.now() },
+		{ id: '2', path: '', name: '', tx: Date.now() },
+		{ id: '3', path: '', name: '', tx: Date.now() }
+	];
+
+	$: selectedItemID = '';
 </script>
 
+<svelte:window
+	on:click={(e) => {
+		if (e.target.className) {
+			if (e.target.className.includes('dontHide')) {
+				return;
+			}
+		}
+		selectedItemID = '';
+	}}
+/>
 <Box figmaImport={{ mobile: { top: 0, left: 0, width: '100%', height: '100%' }, desktop: {} }}>
 	<Label
 		figmaImport={{ mobile: { top: 18, left: 12 } }}
@@ -69,6 +104,83 @@
 		{figmaImportConfig}
 		text={'Your Collection'}
 	/>
+	<Box
+		style="overflow-y: scroll; overflow-x: hidden; display: flex; flex-direction: column; align-items: center; justify-content: start; "
+		figmaImport={{ mobile: { top: 98, left: 12, width: 406, height: 705 } }}
+	>
+		{#each collection as item}
+			<Box
+				backgroundColor="#00000000"
+				width="100%"
+				onSelected={() => {
+					selectedItemID = item.id;
+				}}
+				onClick={(e) => {
+					if (e.target.className) {
+						if (e.target.className.includes('dontHide')) {
+							return;
+						}
+					}
+					selectedItemID = '';
+				}}
+				height="12%"
+				backdropFilter="blur(4px)"
+				style="position: relative; min-height: 12%; margin-bottom: 3%;"
+			>
+				{#if selectedItemID === item.id}
+					<Button
+						transitions={getTransition(1)}
+						className="dontHide"
+						hoverOpacityMin={0}
+						hoverOpacityMax={20}
+						label="Hide"
+						verticalFont="12px"
+						width="30%"
+						left="65%"
+						top="10%"
+						height="35%"
+					/>
+					<Button
+						transitions={getTransition(2)}
+						className="dontHide"
+						label="Delete"
+						hoverOpacityMin={0}
+						hoverOpacityMax={20}
+						color={$globalStyle.errorColor}
+						backgroundColor={$globalStyle.errorColor}
+						borderColor={$globalStyle.errorColor}
+						verticalFont="12px"
+						width="30%"
+						left="65%"
+						top="55%"
+						height="35%"
+					/>
+				{/if}
+				<Box
+					width="32%"
+					left="0%"
+					height="100%"
+					style="border-right: solid 1px {$globalStyle.activeColor};"
+				/>
+				<Label
+					align="left"
+					text="Uploaded on:"
+					top="30%"
+					verticalFont="12px"
+					height="10%"
+					width="30%"
+				/>
+				<Label
+					text={getDateFromUnix(item.tx)}
+					top="60%"
+					align="left"
+					verticalFont="12px"
+					height="10%"
+					width="30%"
+				/>
+			</Box>
+		{/each}
+	</Box>
 	<Button
 		verticalFont="15px"
 		backdropFilter="blur(10px)"
